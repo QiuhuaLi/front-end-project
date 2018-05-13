3d5a8c79105e580bb3e5f600368054f2af44c204
李秋华 <36285520+sliiva@users.noreply.github.com>
2018-05-13 23:53:56 +0800

    添加注释

--- progress/circle.js
+++ progress/circle.js
@@ -3,6 +3,7 @@ class circle{
         this.ctx=item.getContext('2d');
         this.begin();
     }
+    //传入默认参数percent等于0
     begin(percent=0){
         this.drawOutside(percent);
         this.drawInside();
@@ -11,25 +12,25 @@ class circle{
     drawOutside(percent){
         let timer=null;
         clearInterval(timer)
-        
-        let lastPercent=this.lastPercent||0;
+        let lastPercent=this.lastPercent||0;//定义lastpercent
         let dis = percent-lastPercent;
+        //将这次输入的百分比和上次的结果百分比作比较
         if(dis==0) return ;
         timer=setInterval(()=>{
             this.ctx.beginPath();
             this.ctx.moveTo(100,100);        
             if (dis > 0){
-                ++lastPercent
+                ++lastPercent;
                 if (percent <= lastPercent) {
-                    this.lastPercent = percent;
+                    this.lastPercent = percent;//修改lastpercent
                     clearInterval(timer);
                 }
                 
             }else{
                 this.ctx.clearRect(0,0,200,200);
-                --lastPercent
+                --lastPercent;
                 if (percent >= lastPercent) {
-                    this.lastPercent = percent;
+                    this.lastPercent = percent;//修改lastpercent
                     clearInterval(timer);
                 }
             }
@@ -41,6 +42,7 @@ class circle{
             this.drawText(percent, lastPercent);
         },10)
     }
+    //画里面的白色图
     drawInside(){
         this.ctx.beginPath();
         this.ctx.moveTo(100,100);
@@ -49,6 +51,7 @@ class circle{
         this.ctx.fillStyle='#fff';
         this.ctx.fill();
     }
+    //在白色图中写字
     drawText(percent, lastPercent){
         this.ctx.font = "20px Georgia";
         this.ctx.fillStyle='#5E97F6';
